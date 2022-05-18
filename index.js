076ffa281075780cca3878fbcb76d51005eb0c94
guitarkeegan <keegananglim@gmail.com>
2022-05-17 22:05:15 -0700

    add character arrays

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Assignment code here
-function generatePassword(){}
+function generatePassword(){
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const upperCaseLetters = lowercaseLetters.map((letter) => letter.toUpperCase());
+    const specialChars = '!@#$%^&*()'.split('');
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
